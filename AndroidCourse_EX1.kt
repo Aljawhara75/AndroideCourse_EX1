b0dd367f606d1130626bb515ad7df5eaab9fdb79
Aljawhara75 <114245447+Aljawhara75@users.noreply.github.com>
2022-10-03 16:34:58 +0300

    Add files via upload

---
+++ AndroidCourse_EX1.kt
@@ -0,0 +1,28 @@
+package study.e.androidcourse
+
+fun main(){
+/* Boolean */
+    var booleanFalse=false;
+    var booleanTrue=true;
+/*  numbers*/
+    var number2:Short=2;
+    var number3:Short=3;
+    var number5:Short=5;
+    var number6:Short=6;
+    var number10:Short=10;
+//    Q1
+    println(booleanFalse && booleanFalse )
+//    Q2
+    println( number10 % number3 )
+//    Q3
+    println(number5 >= number5)
+//    Q4
+    println(booleanFalse || booleanTrue)
+//    Q5
+   println(number5-number3*number2 ) /*if you wanna the subtraction process first println((number4-number5)*number6 )*/
+//    Q6
+    println(number3!=number2)
+//    Q7
+    println((number6+number6)/number3)
+
+}
